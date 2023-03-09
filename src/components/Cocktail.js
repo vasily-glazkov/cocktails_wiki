f0a3fa83023245868c4ad2bcb13fd69db59cafe8
vasily-glazkov <vglazkoff@gmail.com>
2023-03-09 21:24:00 +0300

    git init

---
+++ src/components/Cocktail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Cocktail = () => {
+    return (<div>
+            <h2>cocktail component</h2>
+        </div>)
+}
+
+export default Cocktail
